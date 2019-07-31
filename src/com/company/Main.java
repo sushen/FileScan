1471ef94fabdeb9d04d666e04578e92cac90750a
Sushen Biswas <sushenbiswasaga@gmail.com>
2019-07-31 20:12:26 +0600

    File exist Exception

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,20 @@ public class Main {
     public static void main(String[] args) throws Exception {
 	// write your code here
         File file = new File("pg60020.txt");
-        Scanner fileScanner = new Scanner(file);
-        System.out.println(fileScanner);
+        if (file.exists()){
+            Scanner fileScanner = new Scanner(file);
+            System.out.println(fileScanner);
 
-        int word = 0;
-        while (fileScanner.hasNextLine()){
-            String line = fileScanner.nextLine();
-            word += line.split(" ").length;
-
+            int word = 0;
+            while (fileScanner.hasNextLine()){
+                String line = fileScanner.nextLine();
+                word += line.split(" ").length;
         }
         System.out.println(word);
 
+     }else {
+            System.out.println("File not exist");
+        }
 
-    }
+  }
 }
